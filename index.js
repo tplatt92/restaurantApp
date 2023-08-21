2facfa02e2218ee3d8e61bfe211ae24eca7adb5e
Tom Platt <thomasjplatt0107@gmail.com>
2023-08-21 09:50:06 +0100

    basic styling

--- index.js
+++ index.js
@@ -17,6 +17,42 @@
 
 import {menuArray} from "./data.js"
 
+/*
+document.addEventListener('click', function(e){
+    if(e.target.dataset.plus){
+       handlePlusClick(e.target.dataset.plus) 
+    }
+    else if(e.target.dataset.remove){
+        handleRemoveClick(e.target.dataset.remove)
+    }
+
+    function handlePlusClick(itemId){ 
+        const targetItemObj = menuArray.filter(function(item){
+            return item.id === itemId
+        })}
+       /* if (targetItemObj){
+
+            let orderHtml = ''
+            orderHtml += 
+            `
+            <div class="order">
+                <h2>Your Order</h2>
+                <div class="order-inner">
+                    <p class="item">${item.name}</p>
+                    <a class="remove" data-remove>remove</a>
+                    <p class="price">${item.price}</p>
+                    <p>Total Price</p>
+                    <p class="total">${totalPrice}</p>
+                    <i class="fa-solid fa-plus" data-plus="${item.id}"></i>
+                </div>
+            <div>
+                } else if (remove button click) {
+                    orderHtml = ''
+                }`
+                render()
+                }   
+
+                */
 
 function getOrderHtml() {
     let menuHtml = ''
@@ -24,32 +60,28 @@ function getOrderHtml() {
     menuHtml+= `
     <div class = "menu">
         <div class="menu-inner">
-            <img src="">
-            <p class="item">${item.name}</p>
-            <p class="ingredients">${item.ingredients}</p>
-            <p class="price">${item.price}</p>
+                <div class="emoji-container>
+                <span class="emoji">${item.emoji}</span>
+                </div>
+                <div class="itemInfo">
+                    <p class="item">${item.name}</p>
+                    <p class="ingredients">${item.ingredients}</p>
+                    <p class="price">${item.price}</p>
+                    </div>
+                <div class="plus-container">
+                    <i class="fa-solid fa-plus" data-plus="${item.id}"></i>
+                </div>
 
+                <div>
+            </div>
         </div>
     </div>
     `
+
     })
     return menuHtml
-   /* let orderHtml = ''
-    orderHtml += 
-    `
-    <div class="order">
-        <h2>Your Order</h2>
-        <div class="order-inner">
-            <p class="item">${item.name}</p>
-            <a class="remove">remove</a>
-            <p class="price">${item.price}</p>
-            <p>Total Price</p>
-            <p class="total">${totalPrice}</p>
-            <i class="fa-solid fa-plus" data-plus="${item.id}"></i>
-        </div>
-    <div>
-    `
-        */
+   
+
     
 }
 
